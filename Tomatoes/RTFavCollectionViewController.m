84fcd61b93f4e58d3f57ebfb6ea40068e40753eb
Terry Bu <burea1124@gmail.com>
2015-01-17 17:17:21 -0500

    Find and replaced created-by-aditya name to me

--- Tomatoes/RTFavCollectionViewController.m
+++ Tomatoes/RTFavCollectionViewController.m
@@ -2,7 +2,7 @@
 //  RTFavCollectionViewController.m
 //  Tomatoes
 //
-//  Created by Aditya Narayan on 1/17/15.
+//  Created by Terry Bu on 1/17/15.
 //  Copyright (c) 2015 Matt Greenwell. All rights reserved.
 //
 
