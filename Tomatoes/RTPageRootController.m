579a0ea4600d7b7044839b8d193d6fb292cc23ef
Terry Bu <burea1124@gmail.com>
2015-01-17 19:58:22 -0500

    pagecontrol implemented

--- Tomatoes/RTPageRootController.m
+++ Tomatoes/RTPageRootController.m
@@ -14,9 +14,9 @@
 
 @property (nonatomic, strong) NSArray *viewControllers;
 @property (nonatomic, strong) UIPageViewController *pageViewController;
-@property (strong, nonatomic) UINavigationController *firstVC;
-@property (strong, nonatomic) UINavigationController *secondVC;
-
+@property (nonatomic, strong) UINavigationController *firstVC;
+@property (nonatomic, strong) UINavigationController *secondVC;
+@property (nonatomic, strong) UIPageControl *pageControl;
 @end
 
 @implementation RTPageRootController
@@ -56,15 +56,18 @@
     
     [self addUniqueObserver:self selector:@selector(favStarWasPressed) name:@"favStarPressed" object:nil];
     [self addUniqueObserver:self selector:@selector(searchBarButtonWasPressed) name:@"searchBarButtonPressed" object:nil];
+    
+    //Specific to dotted pageControl below navigation bar
+    self.pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(self.view.frame.size.width/2-15, self.view.frame.size.height-30, 30, 30)];
+    self.pageControl.backgroundColor = [UIColor clearColor];
+    self.pageControl.pageIndicatorTintColor = [UIColor grayColor];
+    self.pageControl.currentPageIndicatorTintColor = [UIColor whiteColor];
+    [self.pageControl setNumberOfPages:2];
+    [self.view addSubview:self.pageControl];
 
 }
 
-- (void) favStarWasPressed {
-    [self.pageViewController setViewControllers:@[self.secondVC] direction:UIPageViewControllerNavigationDirectionForward animated:YES completion:nil];
-}
-- (void) searchBarButtonWasPressed {
-    [self.pageViewController setViewControllers:@[self.firstVC] direction:UIPageViewControllerNavigationDirectionReverse animated:YES completion:nil];
-}
+
 
 
 - (void)didReceiveMemoryWarning {
@@ -72,26 +75,50 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+#pragma mark - Notifications Related
 - (void)addUniqueObserver:(id)observer selector:(SEL)selector name:(NSString *)name object:(id)object {
     
     [[NSNotificationCenter defaultCenter] removeObserver:observer name:name object:object];
     [[NSNotificationCenter defaultCenter] addObserver:observer selector:selector name:name object:object];
-    
+}
+
+- (void) favStarWasPressed {
+    [self.pageViewController setViewControllers:@[self.secondVC] direction:UIPageViewControllerNavigationDirectionForward animated:YES completion:nil];
+    [self.pageControl setCurrentPage:1];
+}
+- (void) searchBarButtonWasPressed {
+    [self.pageViewController setViewControllers:@[self.firstVC] direction:UIPageViewControllerNavigationDirectionReverse animated:YES completion:nil];
+    [self.pageControl setCurrentPage:0];
 }
 
 #pragma mark - Page View Controller Data Source
 - (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController
 {
-    if (self.pageViewController.viewControllers[0] == self.secondVC)
+    if (self.pageViewController.viewControllers[0] == self.secondVC) {
         return self.firstVC;
+    }
     return nil;
 }
 
 - (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController
 {
-    if (self.pageViewController.viewControllers[0] == self.firstVC)
+    if (self.pageViewController.viewControllers[0] == self.firstVC) {
         return self.secondVC;
+    }
     return nil;
+}
+
+- (void)pageViewController:(UIPageViewController *)pageViewController didFinishAnimating:(BOOL)finished previousViewControllers:(NSArray *)previousViewControllers transitionCompleted:(BOOL)completed
+{
+    UINavigationController *currentNavController = pageViewController.viewControllers[0];
+    UIViewController *topVC = currentNavController.topViewController;
+    
+    if ([topVC isKindOfClass:[RTFavCollectionViewController class]])
+        [self.pageControl setCurrentPage:1];
+    else if ([topVC isKindOfClass:[RTSearchViewController class]])
+        [self.pageControl setCurrentPage:0];
+    
 }
 
 
